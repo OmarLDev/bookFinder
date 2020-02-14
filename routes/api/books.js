4771400ad2bf43ef93c317d65374f57987d79c3a
Omar Luna <mrebel62@gmail.com>
2020-02-14 13:30:11 -0600

    DB structure v1

---
+++ routes/api/books.js
@@ -0,0 +1,14 @@
+const router = require("express").Router();
+const booksController = require("../../controllers/booksController");
+
+// Matching with "/api/books"
+router.route("/")
+    .get(booksController.findAll)
+    .post(booksController.create);
+
+router.route("/:id")
+    .get(booksController.findById)
+    .put(booksController.update)
+    .delete(booksController.remove);
+
+module.exports = router;
