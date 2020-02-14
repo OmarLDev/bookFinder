4747cc405203304bae4165d12f61b83b83866682
Omar Luna <mrebel62@gmail.com>
2020-02-14 12:12:03 -0600

    initial structure

---
+++ create-react-express/client/build/precache-manifest.3223adae75ad5ab4eb8baf187616a8db.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "495d93cc2c05774c4e88882137125113",
+    "url": "/static/media/logo.495d93cc.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c8702f7b2d9b934eae6c",
+    "url": "/static/js/main.d24a8bb8.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "c8702f7b2d9b934eae6c",
+    "url": "/static/css/main.d70f4ff6.chunk.css"
+  },
+  {
+    "revision": "d1359af1498b8f0eed29554638debc32",
+    "url": "/index.html"
+  }
+];
