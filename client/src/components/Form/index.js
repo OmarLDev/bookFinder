d0c4eeb8eb1000897d59ed41003c3b0f45062c2b
Omar Luna <mrebel62@gmail.com>
2020-02-17 17:23:12 -0600

    API search complete

--- client/src/components/Form/index.js
+++ client/src/components/Form/index.js
@@ -6,10 +6,18 @@ function Form(props){
         <div className="container">
             <form>
                 <div className="form-group">
-                    <label for="bookSearch">Book Title</label>
-                    <input type="text" className="form-control" id="bookSearch" placeholder="Book title"/>
+                    <label>Book Title</label>
+                    <input type="text" 
+                            className="form-control" 
+                            id="bookSearch" 
+                            placeholder="Book title"
+                            value={props.value}
+                            name="search"
+                            onChange={props.handleInputChange}/>
                 </div>
-                <button type="submit" className="btn btn-primary">Search</button>
+                <button type="submit" 
+                        className="btn btn-primary"
+                        onClick={props.handleFormSubmit}>Search</button>
             </form>
         </div>
     );
--- client/src/components/Wrapper/index.js
+++ client/src/components/Wrapper/index.js
@@ -8,20 +8,37 @@ class Wrapper extends React.Component{
         search: ""
     }
 
+    // Function to search for books in the Google Books API
     searchBooks = bookName => {
         API.searchBooks(bookName)
             .then(res => this.setState({result: res.data}))
             .catch(err => console.log(err))
     }
 
+    // Initializing with an empty search
     componentDidMount = () => {
         this.searchBooks("");
     }
 
+    // Function to handle the search state
+    handleInputChange = (event) => {
+        const {name, value} = event.target;
+        this.setState({
+            [name]: value
+        });
+    };
+
+    // Function to handle the actual search
+    handleFormSubmit = (event) => {
+        event.preventDefault();
+        this.searchBooks(this.state.search)
+    };
+
     render(){
         return(
-            <Form/>
-            {}
+            <Form 
+                handleInputChange = {this.handleInputChange}
+                handleFormSubmit = {this.handleFormSubmit}/>
         )
     };
 }
